85290735de5924efefa0380d9a3f978e8ba5c480
一壶水 <nian.han@advantech.com.cn>
2022-12-14 10:18:31 +0800

    完善后台管理

---
+++ TPro.Web/Areas/Admin/Controllers/LogManageController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TPro.Web.Areas.Admin.Controllers
+{
+    public class LogManageController : AdminBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RequestLog()
+        {
+            return View();
+        }
+
+        public IActionResult ExceptionLog()
+        {
+            return View();
+        }
+    }
+}
---
+++ TPro.Web/Areas/Admin/Controllers/SysManageController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TPro.Web.Areas.Admin.Controllers
+{
+    public class SysManageController : AdminBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult JurisdictionManage()
+        {
+            return View();
+        }
+
+        public IActionResult UserManage()
+        {
+            return View();
+        }
+
+        public IActionResult RoleManage()
+        {
+            return View();
+        }
+
+        public IActionResult MemoryManage()
+        {
+            return View();
+        }
+    }
+}
